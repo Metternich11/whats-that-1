16e08817d2cbbfd6ce301eee9ffa4b272c18fdbc
Freddie Smith <fs@aeonian-tech.com>
2019-05-06 18:43:42 +0200

    feat(ComponentCatalogue): Introduced component catalogue files

---
+++ client/src/components/Button.js
@@ -0,0 +1,26 @@
+import styled, { css } from "styled-components";
+
+const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+
+  ${props =>
+    props.primary &&
+    css`
+      background: palevioletred;
+      color: white;
+    `};
+
+  ${props =>
+    props.secondary &&
+    css`
+      background: blue;
+      color: white;
+    `};
+`;
+
+export default Button;
---
+++ client/src/containers/ComponentsCatalogue.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Button from "../components/Button";
+
+export const ComponentsCatalogue = () => {
+  return (
+    <>
+      <h2>Buttons</h2>
+      <Button>Hello Gelena</Button>
+      <Button primary>Hello Gelena</Button>
+      <Button secondary>Hello Gelena</Button>
+
+      <h2>Text Style (Not components)</h2>
+      <hr />
+      <h1>Header 1</h1>
+      <p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum</p>
+      <h2>Header 2</h2>
+      <p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum</p>
+      <small>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum</small>
+      <h3>Header 3</h3>
+      <p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum</p>
+      <h4>Header 4</h4>
+      <p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum</p>
+      <h5>Header 5</h5>
+      <p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum</p>
+    </>
+  );
+};
+
+export default ComponentsCatalogue;
