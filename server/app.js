a99cd9d685e04fa029cb8c1ee65b60013c00ae5d
Shanshan Chen <shanshanc.chen@gmail.com>
2019-05-06 12:02:23 +0200

    add a controller

--- server/app.js
+++ server/app.js
@@ -3,11 +3,14 @@ const app = new Koa();
 
 const cors = require("kcors");
 const bodyparser = require("koa-body");
-
+const router = require("./router");
 const port = 3000;
 
 function create() {
-  app.use(cors()).use(bodyparser());
+  app
+    .use(cors())
+    .use(bodyparser())
+    .use(router.routes());
   return app.listen(port, () => {
     console.log(`🚀 Server Running on ${port}`); //eslint-disable-line
   });
