573cafc541adf9945d2cc1267fa8187f0f802409
Gelena Barker <boldireg@tcd.ie>
2019-05-06 21:08:32 +0200

    styled button & navbar

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -3,23 +3,28 @@ import styled, { css } from "styled-components";
 const Button = styled.button`
   background: transparent;
   border-radius: 3px;
-  border: 2px solid palevioletred;
-  color: palevioletred;
+  border: 2px solid;
+  color: white;
   margin: 0 1em;
-  padding: 0.25em 1em;
+  padding: 0.5em 1em;
+  font-size: 16px;
+  :hover {
+    color: grey;
+  }
 
   ${props =>
     props.primary &&
     css`
-      background: palevioletred;
-      color: white;
+      background: #ff87ad;
+      border-color: #e6a8bc;
     `};
 
   ${props =>
     props.secondary &&
     css`
-      background: blue;
-      color: white;
+      background-color: #FA709A;
+      background-image: linear-gradient(90deg, #FA709A 0%, #f5c964 87%);
+      border-color: pink;
     `};
 `;
 
--- client/src/containers/ComponentsCatalogue.js
+++ client/src/containers/ComponentsCatalogue.js
@@ -1,27 +1,17 @@
 import React from "react";
 import Button from "../components/Button";
+import NavBar from "../components/NavBar";
 
 export const ComponentsCatalogue = () => {
   return (
     <>
       <h2>Buttons</h2>
-      <Button>Hello Gelena</Button>
-      <Button primary>Hello Gelena</Button>
-      <Button secondary>Hello Gelena</Button>
-
-      <h2>Text Style (Not components)</h2>
-      <hr />
-      <h1>Header 1</h1>
-      <p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum</p>
-      <h2>Header 2</h2>
-      <p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum</p>
-      <small>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum</small>
-      <h3>Header 3</h3>
-      <p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum</p>
-      <h4>Header 4</h4>
-      <p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum</p>
-      <h5>Header 5</h5>
-      <p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum</p>
+      <Button>default</Button>
+      <Button primary>primary</Button>
+      <Button secondary>secondary</Button>
+      <h2>NavBar</h2>
+      <NavBar>Logo and name</NavBar>
+      <h2>Input fields</h2>
     </>
   );
 };
