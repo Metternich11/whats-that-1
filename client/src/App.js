444174f50ddc5099198a0c8100d349ddecff1604
Freddie Smith <fs@aeonian-tech.com>
2019-05-06 18:38:02 +0200

    feat(Text Stylings): Wrote styling for fonts within app

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,37 @@ import ComponentsCatalogue from "./containers/ComponentsCatalogue";
 
 const GlobalStyle = createGlobalStyle`
   body {
-    @import url("https://fonts.googleapis.com/css?family=Nunito:400,600");
     background-color: #764ad7;
     color: #ffffff;
     margin: 0;
+    @import url("https://fonts.googleapis.com/css?family=Nunito:400,600");
     font-family: 'Nunito', sans-serif;
-    font-size: 16px;
-    text-align: center;
   }
+
+  html {
+    font-size: 16px;
+  }
+
+  h1, h2, h3, h4, h5 {
+    margin: 2.75rem 0 1rem;
+    font-weight: 600;
+    line-height: 1.15;
+  }
+
+  h1 {
+    margin-top: 0;
+    font-size: 3.052em;
+  }
+
+  h2 {font-size: 2.441em;}
+
+  h3 {font-size: 1.953em;}
+
+  h4 {font-size: 1.563em;}
+
+  h5 {font-size: 1.25em;}
+
+  small {font-size: 0.8em;}
 `;
 
 function App() {
