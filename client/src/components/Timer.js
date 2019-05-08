ab083f9fe822ab2dce3e15fe6b490c73d9547824
Freddie Smith <fs@aeonian-tech.com>
2019-05-08 12:20:52 +0200

    feat(Game): Added components which will be used in the Game view

---
+++ client/src/components/Timer.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const Timer = styled.div`
+  color: white;
+  font-weight: bold;
+  align-self: center;
+`;
+
+export default Timer;
---
+++ client/src/components/WordToDraw.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const WordToDraw = styled.div`
+  color: white;
+`;
+
+export default WordToDraw;
