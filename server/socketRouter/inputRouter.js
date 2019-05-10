d847f7aaee5b73f15d1cbdb419f7afad611ceada
Ole Hauer <ole@Oles-MBP.lan>
2019-05-10 21:07:22 +0200

    triangle-setup-basic

--- server/socketRouter/inputRouter.js
+++ server/socketRouter/inputRouter.js
@@ -1,27 +1,32 @@
 const gameController = require('../controllers/gameController');
+const socketArray = [];
 
 const inputRouter = io => {
     io.on('connect', socket => {
-    socket.on('clientAction', message => {
-      switch (message.type) {
-        case 'createRoom':
-          gameController.createGame(socket, message);
-          break;
-        case 'joinRoom':
-          gameController.joinRoom(socket, message);
-          break;
-        case 'receiveDraw':
-          gameController.receiveDraw(socket, message);
-          break;
-        case 'startGame': 
-          gameController.startGame(socket, message);
-          break;
-        case 'leaveRoom':
-          gameController.leaveRoom(socket, message);
-          break;
-      }
-    })
-  });
+      socketArray.push(socket.id);
+      socket.on('clientAction', message => {
+        switch (message.type) {
+          case 'createRoom':
+            gameController.createGame(socket, message);
+            break;
+          case 'joinRoom':
+            gameController.joinRoom(socket, message);
+            break;
+          case 'receiveDraw':
+            gameController.receiveDraw(socket, message);
+            break;
+          case 'receiveFinalDraw':
+            gameController.receiveFinalDraw(socket, message);
+            break;
+          case 'startGame': 
+            gameController.startGame(socket, message);
+            break;
+          case 'leaveRoom':
+            gameController.leaveRoom(socket, message);
+            break;
+        }
+      })
+    });
 }
 
 module.exports = inputRouter;
