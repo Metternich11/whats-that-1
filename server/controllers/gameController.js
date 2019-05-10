0754f84ea1552123fce1d81e9098826c66c06997
Shanshan Chen <shanshanc.chen@gmail.com>
2019-05-10 21:08:22 +0200

    feat: work-in-progress gameController

--- server/controllers/gameController.js
+++ server/controllers/gameController.js
@@ -1,67 +1,90 @@
+ /* eslint-disable no-console */
 const outputRouter = require('../socketRouter/outputRouter')
 const gameModel = require('../models/gameModel');
+const requestWords = require('../helpers/requestWords');
+const requestGuess = require('../helpers/requestGuess');
 
-const gameController = {
-  createGame: (socket, message) => {
-    if(gameModel.addGame(message.payload.key, socket.id)) {
-      gameModel.addPlayer(message.payload, socket.id);
-      outputRouter.join(socket, message.payload.key);
+const RoundsPerGame = 3;
+const MillisecondsPerRound = 23000;
+
+const GameController = () => {
+  return {
+    game: {
+      currentRound: 0,
+      words: [],
+      isCurrentRoundComplete: false
+    },
+    timer: () => {
+      return setTimeout(() => 
+        this.endRound()
+      , MillisecondsPerRound)
+    },
+    startRound: () => {
+      this.game.isCurrentRoundComplete = false;
+      this.timer = this.roundTimer();
+    },
+    endRound: () => {
+      this.game.isCurrentRoundComplete = true;
+      this.game.currentRound += 1;
+    },
+    getWords: () => {
+      this.game.words = requestWords(RoundsPerGame);
+    },
+    getCurrentWord: (currentRound) => {
+      return this.game.words[currentRound];
+    },
+    getCurrentRound: () => {
+      this.game.currentRound = gameModel.getCurrentRound();
+    },
+
+    // for inputRouter and outputRouter
+    createGame: (socket, message) => {
+      try {
+        gameModel.addGame(message.payload.key, socket.id, RoundsPerGame);
+        gameModel.addPlayerToGame(message.payload.key, socket.id, true);
+        gameModel.addPlayer(message.payload, socket.id);
+        outputRouter.join(socket, message.payload.key);
+        outputRouter.sendMessageRoom(socket, message);
+      } catch(err) {
+        console.error(err); 
+      }
+    },
+    joinRoom: (socket, message) => {
+      try {
+        gameModel.addPlayer(socket.id);
+        outputRouter.sendMessageRoom(socket, message);
+      } catch (err) {
+        console.error(err);
+      }
+    },
+    startGame: (socket, message) => {
+      if (gameModel.gameExists) {
+        gameModel.startGame(socket.id.key); // double-check if that's the right way to get the key
+      } else {
+        outputRouter.gameDoesNotExist(); // need to have this func in outputRouter
+      }
+    },
+    receiveDraw: (socket, message) => {
+      const currentWord = gameModel.getCurrentWord();
+      const guess = requestGuess(message.payload);
+      outputRouter.sendMessagePlayer(socket, {guess});
+      if (guess === currentWord) {
+        gameModel.setPlayerRoundWins(socket.id);
+      }
+      // check if the round is active. if not, add the drawing to player's draws
+      if(this.game.isCurrentRoundComplete()) {
+        this.game.addDrawing();
+              clearTimeout(this.timer);
+              this.endRound();
+            }
+    },
+    leaveRoom: (socket, message) => {
+      gameModel.removePlayer(socket.id);
       outputRouter.sendMessageRoom(socket, message);
-    }
+    },
   }
-}
+};
 
-// const gameModel = require('../models/gameModel')
-
-// class GameController {
-//   constructor(game, socketController) {
-//     this.game = game;
-//     this.socket = socketController;
-
-//     this.endRound = this.endRound.bind(this);
-//   }
-
-//   startGame () {
-//     //setup game
-//     try{
-//       startRound();
-//     } catch (e) {
-//       if(typeof e === GameNotEnoughPlayersError) {
-//         socket.sendNotEnoughPlayers();
-//       }
-//     }
-//   };
-
-//   adminJoin () {};
-//   playerJoin () {};
-
-
-//   startRound () {
-//     this.game.startNewRound(); // tells the game to increase currentRound (figures out the new word)
-//     socket.sendStart(this.game.currentRound); // sends the info for this round
-//     this.timer = setTimeout(this.endRound, 26000);
-//   };
-
-//   endRound() {
-//     socket.sendEndRound();
-//     if (this.game.isLastRound()) {
-//       this.endGame();
-//     } else {
-//       this.startRound();
-//     }
-//   };
-
-//   endGame () {
-//     socket.sendEndGame(this.game);
-//   };
-
-//   receiveDrawing(drawing) {
-//     this.game.addDrawing();
-//     if(this.game.isCurrentRoundComplete()) {
-//       clearTimeout(this.timer);
-//       this.endRound();
-//     }
-//   };
-// }
-
-module.exports = gameController;
+// update inputRouter how to call it
+//const gameController = GameController();
+module.exports = GameController;
