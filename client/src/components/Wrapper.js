c280e4813c1c271f468993b2ff132ea43883c36c
Freddie Smith <fs@aeonian-tech.com>
2019-05-13 15:11:58 +0200

    feat(ComponentsCatalogue): All components used within the app are added

--- client/src/components/Wrapper.js
+++ client/src/components/Wrapper.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 const Wrapper = styled.div`
   align-items: center;
@@ -15,6 +15,48 @@ const Wrapper = styled.div`
   @media (max-width: 1024px) {
     margin: 0 0 5vh 0;
   }
+
+  ${props =>
+    props.ComponentsCatalogue &&
+    css`
+      align-items: flex-start;
+      padding: 0 5vw;
+      text-align: left;
+
+      .components {
+        border: 1px solid rgba(255, 255, 255, 0.2);
+        border-radius: 3px;
+        margin-bottom: 5vh;
+        padding: 2vw;
+        padding-left: 5vw;
+        width: 100%;
+      }
+
+      .divider {
+        margin: 100px 0;
+        width: 100%;
+        border: 2px solid;
+        border-color: #764ad7;
+      }
+
+      h1 {
+        color: #472c81;
+        font-size: 3rem;
+      }
+      h2 {
+        color: #472c81;
+        font-size: 2.2rem;
+      }
+
+      .large {
+        font-size: 1.4rem;
+      }
+
+      h4 {
+        border-bottom: 1px solid rgba(255, 255, 255, 0.2);
+        padding-bottom: 5px;
+      }
+    `};
 `;
 
 export default Wrapper;
