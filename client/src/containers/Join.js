d9208de13f77b5bce7018bb5d4c031bcf78665e8
gibsoncream <konstanmor@gmail.com>
2019-05-14 18:35:59 +0200

    feat: avatars changing dynamically on user select

--- client/src/containers/Join.js
+++ client/src/containers/Join.js
@@ -15,14 +15,18 @@ import PlayerAvatar from '../components/PlayerAvatar';
 import SpeechBubble from '../components/SpeechBubble';
 import Wrapper from '../components/Wrapper';
 
-const Join = ({ game, pages, connectGame, history }) => {
+// Util imports
+import generateAvatarProps from "../utils/generateAvatarProps";
+
+
+const Join = ({ game, pages, connectGame, history, currentUser }) => {
   const [playerName, setPlayerName] = useState('');
   const [gameKey, setGameKey] = useState('');
+  const [userChoice, setUserChoice] = useState("");
   const joinForm = useRef();
   const gameKeyInput = useRef();
 
   useEffect(() => {
-      console.log('ITS SUPER EFFECTIVE!', game.players)
       if (game.players) history.push('/lobby');
     }, [game]);
 
@@ -42,17 +46,22 @@ const Join = ({ game, pages, connectGame, history }) => {
       const value = event.target.value;
       setGameKey(value);
       gameKeyInput.current.setCustomValidity('');
-      console.log('he ho')
   };
 
   const submitAndConnect = e => {
     e.preventDefault();
-    connectGame(playerName, game.userAvatar, gameKey, 'joinGame');
+    connectGame(playerName, currentUser.userAvatar, gameKey, 'joinGame');
   };
 
   const goBack = () => {
     history.goBack();
   };
+
+  const refreshAvatar = (e) => {
+    e.preventDefault();
+    let props = generateAvatarProps();
+    setUserChoice(props);
+  }
 
   return (
     <Wrapper>
@@ -63,10 +72,10 @@ const Join = ({ game, pages, connectGame, history }) => {
           <AvatarContainer
             style={{ transform: 'scale(2.5)', marginTop: '2vh' }}
           >
-            <PlayerAvatar />
+            <PlayerAvatar userChoice={userChoice} />
           </AvatarContainer>
         </div>
-        <Button refresh>
+        <Button refresh onClick={refreshAvatar}>
           <i className='fas fa-sync-alt' />
         </Button>
         <FormLabel>Name</FormLabel>
@@ -104,7 +113,8 @@ const mapStateToProps = state => {
   console.log('STATE', state);
   return {
     game: state.game,
-    pages: state.pages
+    pages: state.pages,
+    currentUser: state.currentUser
   };
 };
 
