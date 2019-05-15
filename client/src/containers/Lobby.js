499273e503bd92dbe8de9b733e51a2f89fa1c5ba
Arol <arolet@gmail.com>
2019-05-15 12:26:27 +0200

    feat: added share button in the lobby

--- client/src/containers/Lobby.js
+++ client/src/containers/Lobby.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
-import { TelegramShareButton, TelegramIcon } from 'react-share'; 
+import { TelegramShareButton, TelegramIcon } from 'react-share';
+import styled from 'styled-components/macro';
 
 // Redux Imports
 import { connect } from 'react-redux';
@@ -14,13 +15,35 @@ import PlayerAvatar from '../components/PlayerAvatar';
 import PlayerList from '../components/PlayerList';
 import PlayerListItem from '../components/PlayerListItem';
 import Wrapper from '../components/Wrapper';
+import DeviceDetector from '../utils/deviceDetector';
 
-export const Lobby = (props) => {
+const host = window.location.protocol + '//' + window.location.host;
+
+const ShareIcon = styled.i`
+  margin-top: 20px;
+  font-size: 2em;
+`;
+
+const ShareButton = ({ url }) => {
+  if (typeof window.navigator.share !== 'function') return null;
+
+  const share = () => {
+    if (typeof window.navigator.share === 'function') {
+      window.navigator.share({ url });
+    }
+  };
+
+  let icon = DeviceDetector.isAndroid() ? 'fa-share-alt' : 'fa-share-square';
+
+  return <ShareIcon onClick={share} className={`fas ${icon}`} />;
+};
+
+export const Lobby = props => {
   const startGame = () => {
     props.startGame();
   };
 
-  const shareUrl = `http://localhost:3000/${props.currentUser.gameKey}`;
+  const shareUrl = `${host}/${props.currentUser.gameKey}`;
 
   const opponents = props.game.players;
 
@@ -31,15 +54,15 @@ export const Lobby = (props) => {
   return (
     <Wrapper>
       <GameHeader>
-        <h2 className='gameHeader'>{props.currentUser.gameKey}</h2>
+        <h2 className="gameHeader">{props.currentUser.gameKey}</h2>
         {props.currentUser.isCreator === 'createGame' ? (
           <Button primary onClick={startGame}>
             {' '}
             Start!{' '}
           </Button>
         ) : (
-            ''
-          )}
+          ''
+        )}
       </GameHeader>
       <p>Practice drawing whilst waiting...</p>
 
@@ -47,7 +70,12 @@ export const Lobby = (props) => {
       <p>Waiting for other players...</p>
       <PlayerList>
         <PlayerListItem>
-          {opponents && Object.values(opponents).filter(player => player.playerId !== props.currentUser.userId).map((player, index) => <PlayerAvatar key={index} info={player} />)}
+          {opponents &&
+            Object.values(opponents)
+              .filter(player => player.playerId !== props.currentUser.userId)
+              .map((player, index) => (
+                <PlayerAvatar key={index} info={player} />
+              ))}
         </PlayerListItem>
       </PlayerList>
       <AvatarShelf>Your Opponents</AvatarShelf>
@@ -57,13 +85,9 @@ export const Lobby = (props) => {
           Start!{' '}
         </Button>
       ) : (
-          ''
-        )}
-      {
-        true 
-          ? <button onClick={() => window.navigator.share({url: 'https://google.com'})}>Share</button>
-          : null
-      }
+        ''
+      )}
+      <ShareButton url={shareUrl} />
     </Wrapper>
   );
 };
--- client/src/utils/deviceDetector.js
+++ client/src/utils/deviceDetector.js
@@ -1,13 +1,11 @@
-const DeviceDetector = () => {
-  return {
-    userAgent: () => navigator.userAgent || navigator.vendor || window.opera,
-    isIOS: () => {
-      return /iPad|iPhone|iPod/.test(this.userAgent()) && !window.MSStream
-    },
-    isAndroid: () => {
-      return /android/i.test(this.userAgent())
-    }
+const DeviceDetector = {
+  userAgent: () => navigator.userAgent || navigator.vendor || window.opera,
+  isIOS: function() {
+    return /iPad|iPhone|iPod/.test(this.userAgent()) && !window.MSStream;
+  },
+  isAndroid: function() {
+    return /android/i.test(this.userAgent());
   }
-}
+};
 
 export default DeviceDetector;
