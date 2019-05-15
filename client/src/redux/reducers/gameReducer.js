2e0bb7062b61a824a2c0dfe409565218f741a415
gibsoncream <konstanmor@gmail.com>
2019-05-15 18:10:38 +0200

    feat: mad arol redux skilz

--- client/src/redux/reducers/gameReducer.js
+++ client/src/redux/reducers/gameReducer.js
@@ -7,7 +7,8 @@ const initialState = {
   inBetweenRounds: false,
   endGame: false,
   guess: '',
-  winners: []
+  winners: [],
+  rounds: []
 };
 
 export default (state = initialState, action) => {
@@ -40,16 +41,118 @@ export default (state = initialState, action) => {
           ...state,
           guess: action.payload.payload.word
         };
-      case SocketTypes.VICTORY:
+      case SocketTypes.VICTORY: {
+        const data = action.payload.payload;
         return {
           ...state, 
-          winners: [...state.winners, action.payload.payload.playerId]
+          rounds: [
+            ...state.rounds,
+            {
+              roundNum: data.round.currentRound,
+              winners: data.players.filter(player => {
+                const round = player.roundWins.find(round => round.round === data.round.currentRound);
+                return round && round.winner;
+              }).map(player => player.id),
+            }
+            // {
+            //     action.payroundNum,
+            //     winners: [winnerId],
+            //     // drawings: [{
+            //     //   svg,
+            //     //   author
+            //     // }]
+            // }
+          ]
+          
+          // action.payload.payload.players.filter((player) => {
+          //   return player.roundWins
         };
-      case SocketTypes.ROUND_DRAWINGS:
+      }
+
+      // {
+      //   type: 'SOCKET_MESSAGE',
+      //   payload: {
+      //     type: 'roundDrawings',
+      //     payload: {
+      //       round: {
+      //         currentRound: 1,
+      //         word: 'helmet',
+      //         roundStatus: false
+      //       },
+      //       playing: false,
+      //       players: [
+      //         {
+      //           playerName: 'asdf',
+      //           playerAvatar: {
+      //             avatarStyle: 'Circle',
+      //             topType: 'LongHairBob',
+      //             accessoriesType: 'Round',
+      //             hairColor: 'Blonde',
+      //             facialHairType: 'Blank',
+      //             facialHairColor: 'Brown',
+      //             clotheType: 'GraphicShirt',
+      //             clotheColor: 'Pink',
+      //             graphicType: 'Pizza',
+      //             eyeType: 'EyeRoll',
+      //             eyebrowType: 'Default',
+      //             mouthType: 'Twinkle',
+      //             skinColor: 'Tanned'
+      //           },
+      //           roundWins: [
+      //             {
+      //               round: 1,
+      //               winner: true
+      //             }
+      //           ],
+      //           isCreator: true,
+      //           gameKey: 'Cool-Snow',
+      //           draws: [
+      //             {
+      //               round: 1,
+      //               draw: '<svg xmlns="http://www.w3.org/2000/svg" xlink="http://www.w3.org/1999/xlink" height="375" width="375" viewBox="0 0 375 375"></svg>',
+      //               word: 'helmet',
+      //               winner: [
+      //                 {
+      //                   round: 1,
+      //                   winner: true
+      //                 }
+      //               ]
+      //             }
+      //           ],
+      //           id: 'b99X4dfRD6rkAf-ZAAAA'
+      //         }
+      //       ],
+      //       totalRounds: 2,
+      //       gameKey: 'Cool-Snow'
+      //     }
+      //   }
+      // }
+
+      case SocketTypes.ROUND_DRAWINGS: {
+        const data = action.payload.payload;
+        const newState = {
+          ...state,
+          rounds: state.rounds.filter(round => round.roundNum !== data.round.currentRound)
+        };
         return {
           ...state,
-          playerDrawings: action.payload.payload.players
+          playerDrawings: action.payload.payload.players,
+          rounds: [
+                ...state.rounds,
+                {
+                  roundNum: data.round.currentRound,
+                  winners: data.players.filter(player => {
+                    const round = player.roundWins.find(round => round.round === data.round.currentRound);
+                    return round && round.winner;
+                  }).map(player => player.id),
+                  drawings: data.players.map(player => ({
+                      author: player.id,
+                      svg: player.draws.find(draw => draw.round === data.round.currentRound).draw
+                  }))
+                }
+              ]
         };
+      }
       case SocketTypes.GAME_DRAWINGS:
         return {
           ...state,
@@ -77,3 +180,50 @@ export default (state = initialState, action) => {
     return state;
   }
 };
+
+// {
+//   type: 'SOCKET_MESSAGE',
+//   payload: {
+//     type: 'victory',
+//     payload: {
+//       round: {
+//         currentRound: 1,
+//         word: 'lightning',
+//         roundStatus: true
+//       },
+//       playing: false,
+//       players: [
+//         {
+//           playerName: 'asd',
+//           playerAvatar: {
+//             avatarStyle: 'Circle',
+//             topType: 'ShortHairShortFlat',
+//             accessoriesType: 'Blank',
+//             hairColor: 'BlondeGolden',
+//             facialHairType: 'Blank',
+//             facialHairColor: 'BrownDark',
+//             clotheType: 'BlazerShirt',
+//             clotheColor: 'PastelYellow',
+//             graphicType: 'Diamond',
+//             eyeType: 'Wink',
+//             eyebrowType: 'DefaultNatural',
+//             mouthType: 'Grimace',
+//             skinColor: 'Brown'
+//           },
+//           roundWins: [
+//             {
+//               round: 1,
+//               winner: true
+//             }
+//           ],
+//           isCreator: true,
+//           gameKey: 'Wild-Haze',
+//           draws: [],
+//           playerId: '2CZ5wmgztVyfElyvAAAC'
+//         }
+//       ],
+//       totalRounds: 2,
+//       gameKey: 'Wild-Haze'
+//     }
+//   }
+// }
