3dd14d2fbc3839b7e0412db3d1fd11be16ce2b37
Arol <arolet@gmail.com>
2019-05-16 17:04:40 +0200

    Updates

---
+++ precache-manifest.f33ca8c46d83894812a6545449137e74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "427b80738029180a2b8ebb2b677722e1",
+    "url": "/whats-that/index.html"
+  },
+  {
+    "revision": "d28d4472738048f65c2c",
+    "url": "/whats-that/static/css/main.f93bb1dc.chunk.css"
+  },
+  {
+    "revision": "462969596ff26728435e",
+    "url": "/whats-that/static/js/2.fb85116e.chunk.js"
+  },
+  {
+    "revision": "d28d4472738048f65c2c",
+    "url": "/whats-that/static/js/main.4052001f.chunk.js"
+  },
+  {
+    "revision": "36c866f900468a180843",
+    "url": "/whats-that/static/js/runtime~main.6ab89fc1.js"
+  },
+  {
+    "revision": "6dc49d5a2e36282e7c0e96742ea835bb",
+    "url": "/whats-that/static/media/Zorb.6dc49d5a.svg"
+  },
+  {
+    "revision": "28ff9026ef28b0828700039c477da70e",
+    "url": "/whats-that/static/media/checkMark.28ff9026.png"
+  }
+]);
